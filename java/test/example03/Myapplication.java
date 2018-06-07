ffada831d924757952c310354f8b81c082f23fc0
zpzhao <zhaozp@uxsino.com>
2018-06-07 17:59:20 +0800

    initial block

---
+++ java/test/example03/Myapplication.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+
+public class Myapplication
+{
+	public static int index;
+	public final String name = "abc";
+	public Employee el;
+
+	public static void main(String[] args)
+	{
+		Myapplication ap = new Myapplication();
+
+		System.out.println("hello world !");
+		System.out.println("index:"+ap.index+",name:"+ap.name+",el.name:"+ap.el.name);
+	}
+	{
+		el = new Employee();
+	}
+}
+
+class Employee
+{
+
+	public int no;
+	public String name;
+	
+	{
+		no = 2;
+		name = "e1";
+	}	
+}
+
