1809166822e43b67ea0c62887ef5043a8d0d5c82
zpzhao <zhaozp@uxsino.com>
2020-10-15 11:00:21 +0800

    # WARNING: head commit changed in the meantime
    java sms4

---
+++ java/spider/src/spider/Spider.java
@@ -0,0 +1,28 @@
+/**
+ * @author zpzhao
+ * @date 2020/10/14
+ */
+package spider;
+
+/**
+ * @author zpzhao
+ *
+ */
+class Spider {
+
+	/**
+	 * 
+	 */
+	public Spider() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
